5eb586082dc1a0495cdcdce5bae76dee6861861c
Ahmad Shah <ahmad.shah@mongodb.com>
2024-01-17 10:26:27 -0500

    remove ascii encoding on results

--- pytpcc/util/results.py
+++ pytpcc/util/results.py
@@ -250,5 +250,5 @@ class Results:
         if driver:
             driver.save_result(result_doc)
         print(result_doc)
-        return ret.encode('ascii', "ignore")
+        return ret
 ## CLASS
