10bfd629d4135ab6bd9d614b7e9cf374951b7de8
Miguel Ángel Nieto <miguel.nieto@mongodb.com>
2025-01-15 11:02:34 +0100

    remove comment

--- pytpcc/drivers/postgresqldriver.py
+++ pytpcc/drivers/postgresqldriver.py
@@ -308,10 +308,6 @@ class PostgresqlDriver(AbstractDriver):
                 ## Commit!
                 self.conn.commit()
 
-                ## Adjust the total for the discount
-                #print "c_discount:", c_discount, type(c_discount)
-                #print "w_tax:", w_tax, type(w_tax)
-                #print "d_tax:", d_tax, type(d_tax)
                 total *= (1 - c_discount) * (1 + w_tax + d_tax)
 
                 ## Pack up values the client is missing (see TPC-C 2.4.3.5)
